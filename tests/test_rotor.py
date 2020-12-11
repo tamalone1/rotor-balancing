ec3463e0b9b16adef352fb1253e801090e5767fb
tamalone1 <65259148+tamalone1@users.noreply.github.com>
2020-12-10 19:36:24 -0600

    Add test directory.

---
+++ tests/test_rotor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+MIT License
+
+Copyright (c) 2020 tamalone1
+"""
+
+import unittest
+from rotor_balancing import *
+
+
+class TestRotor(unittest.TestCase):
+
+    def test_placeholder(self):
+        self.assertTrue(True)
