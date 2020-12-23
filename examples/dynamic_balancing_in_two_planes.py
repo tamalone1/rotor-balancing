3f7cfbb2d63bd3a5abb8f024779db56ea97a41f1
tamalone1 <65259148+tamalone1@users.noreply.github.com>
2020-12-23 11:31:05 -0600

    Add solve method to Rotor class, so it can perform the whole two-plane balance procedure.

--- examples/dynamic_balancing_in_two_planes.py
+++ examples/dynamic_balancing_in_two_planes.py
@@ -5,7 +5,7 @@ Copyright (c) 2020 tamalone1
 """
 import cmath
 from math import radians
-from rotor_balancing.rotor import Rotor
+from rotor_balancing.Rotor import Rotor
 
 # Baseline reactions (length)
 XA = cmath.rect(8.6, radians(63))
@@ -18,18 +18,13 @@ mR = cmath.rect(12, radians(180))
 # Trial reactions using mL on plane L (length)
 XAL = cmath.rect(5.9, radians(123))
 XBL = cmath.rect(4.5, radians(228))
+rotor.add_left_test_mass(XAL, XBL, mL)
 # Trial reactions using mR on plane R (length)
 XAR = cmath.rect(6.2, radians(36))
 XBR = cmath.rect(10.4, radians(162))
-# Stiffness = change in the bearing reaction divided by the trial mass that
-# caused it. One value per bearing per trial mass
-left_stiffnesses = rotor.get_stiffnesses(XAL, XBL, XA, XB, mL)
-right_stiffnesses = rotor.get_stiffnesses(XAR, XBR, XA, XB, mR)
+rotor.add_right_test_mass(XAR, XBR, mR)
 # The existing unbalances and their locations, on planes L and R
-ML, MR = rotor.required_balances(*left_stiffnesses,
-                                 *right_stiffnesses,
-                                 XA,
-                                 XB)
+ML, MR = rotor.solve()
 # Print the result in polar form like the problem statement
-print('ML: {:.03f} <{:4.03f}'.format(*cmath.polar(ML)))
-print('MR: {:.03f} <{:4.03f}'.format(*cmath.polar(MR)))
+print('ML: {:>7.03f} < {:> 4.03f}'.format(*cmath.polar(ML)))
+print('MR: {:>7.03f} < {:> 4.03f}'.format(*cmath.polar(MR)))
--- rotor_balancing/Rotor.py
+++ rotor_balancing/Rotor.py
@@ -13,31 +13,53 @@ class Rotor:
         # Required data: bearing deflections, imbalanced masses
         self.baseline = {'XA': bearing_deflection_A,
                          'XB': bearing_deflection_B}
-        self.test_mass_1 = {}
-        self.test_mass_2 = {}
+        self.test_mass_left_plane = {}
+        self.test_mass_right_plane = {}
 
     def add_imbalance(self, mass_radius, phase):
-        '''Add a mass-radius couple at a particular phase angle.'''
+        """Add a mass-radius couple at a particular phase angle."""
         pass
 
     @staticmethod
     def get_stiffnesses(XAm, XBm, XA, XB, m):
-        ''' Find the stiffnesses in each balance plane due to trial masses.'''
+        """ Find the stiffnesses in each balance plane due to trial masses."""
         A = (XAm - XA)/m
         B = (XBm - XB)/m
         return A, B
 
     @staticmethod
     def required_balances(AL, BL, AR, BR, XA, XB):
-        ''' Find the required balances to add to balance the rotor.'''
+        """ Find the required balances to add to balance the rotor."""
         ML = (XA*BR-AR*XB)/(AL*BR-AR*BL)
         MR = (AL*XB-XA*BL)/(AL*BR-AR*BL)
         return ML, MR
 
+    def add_left_test_mass(self, deflection_A, deflection_B, mL):
+        """ Add the deflections caused by the left deflection mass. """
+        self.test_mass_left_plane = {'XAL': deflection_A,
+                                     'XBL': deflection_B,
+                                     'mL': mL}
+
+    def add_right_test_mass(self, deflection_A, deflection_B, mR):
+        """ Add the deflections caused by the right deflection mass. """
+        self.test_mass_right_plane = {'XAR': deflection_A,
+                                      'XBR': deflection_B,
+                                      'mR': mR}
+
     def solve(self):
-        ''' Find the required balance masses in each balance plane.'''
+        """ Find the required balance masses in each balance plane."""
         # Procedure:
         # Calculate the stiffness of the rotor relative to the test masses,
         # placed at each balancing plane. Using the stiffnesses, calculate the
         # balance mass that will result in zero deflections at the bearings.
-        pass
+        XAL, XBL, mL = self.test_mass_left_plane.values()
+        XAR, XBR, mR = self.test_mass_right_plane.values()
+        XA, XB = self.baseline.values()
+        left_stiffnesses = self.get_stiffnesses(XAL, XBL, XA, XB, mL)
+        right_stiffnesses = self.get_stiffnesses(XAR, XBR, XA, XB, mR)
+        # The existing unbalances and their locations, on planes L and R
+        ML, MR = self.required_balances(*left_stiffnesses,
+                                        *right_stiffnesses,
+                                        XA,
+                                        XB)
+        return ML, MR
